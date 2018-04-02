73a08280183b8dbce6d5ceb97ee9be5f46c596d7
liulian <1203370723@qq.com>
2018-04-02 17:17:09 +0800

    删除不需要文件

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,11 @@
 import Login from './views/Login.vue'
 const NotFound = notFound => require(['./views/404.vue'], notFound)
 const Home = home => require(['./views/Home.vue'], home)
-const Table = table => require(['./views/nav1/Table.vue'], table)
-const Form = form => require(['./views/nav1/Form.vue'], form)
-const User = user => require(['./views/nav1/user.vue'], user)
-const Room = Room => require(['./views/nav2/Room.vue'], Room)
-const Page5 = page5 => require(['./views/nav2/Page5.vue'], page5)
-const Page6 = page6 => require(['./views/nav3/Page6.vue'], page6)
+const Table = table => require(['./views/userInfo/Table.vue'], table)
+const Form = form => require(['./views/userInfo/Form.vue'], form)
+const User = user => require(['./views/userInfo/user.vue'], user)
+const Room = Room => require(['./views/roomManage/Room.vue'], Room)
+const Page5 = page5 => require(['./views/roomManage/Page5.vue'], page5)
 const echarts = echarts => require(['./views/charts/echarts.vue'], echarts)
 let routes = [
     {
@@ -41,16 +40,6 @@ let routes = [
         children: [
             {path: '/room', component: Room, name: '房间信息'},
             {path: '/page5', component: Page5, name: '战绩信息'}
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: '',
-        iconCls: 'fa fa-address-card',
-        leaf: true,//只有一个节点
-        children: [
-            {path: '/page6', component: Page6, name: '导航三'}
         ]
     },
     {
